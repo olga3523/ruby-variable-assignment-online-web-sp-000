2d9f2eb904e184a32e4a9139d840aacd3ca7ad44
olga gomelskiy <olichka23.85@gmail.com>
2020-12-04 00:49:21 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,3 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
+ # ggreetingDefine a local variable called 'greeting',
+ # ggive it the value of the string "Hello World"
+ greeting = "Hello World"
